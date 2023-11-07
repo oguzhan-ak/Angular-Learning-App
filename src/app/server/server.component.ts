95e9922c3f8dc24ed7a45f17c353ecb201b2e6d4
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-11-07 22:14:50 +0300

    bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus(): string {
+    return this.serverStatus;
+  }
+}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created!';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
